feeba4b1facef8056201d0d46a2bd6cee5710734
Alex Towli <alextowli@hotmail.co.uk>
2019-07-13 14:27:31 +0100

    feat(*): youtube api wrapper and enable cors

---
+++ src/modules/youtube.api.wrapper.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+import { config } from 'dotenv';
+
+config();
+
+const YOUTUBE_SEARCH_API_BASE = process.env.YOUTUBE_SEARCH_API_BASE;
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+
+interface IYoutubeOptions {
+  q: string;
+  maxResults: number;
+  part: string;
+  type: string;
+  key: string;
+}
+
+enum Method {
+  Get = 'get',
+  Post = 'post',
+  Delete = 'delete'
+}
+
+export function getYoutubeVideoURL(query: string) {
+  const options: IYoutubeOptions = {
+    q: query,
+    maxResults: 1,
+    part: 'snippet',
+    type: 'video',
+    key: YOUTUBE_API_KEY
+  };
+
+  return makeRequest(YOUTUBE_SEARCH_API_BASE, Method.Get, options).then(res => {
+    return `https://youtube.com/watch?v=${res.data.items[0].id.videoId}`;
+  });
+}
+
+function makeRequest(url: string, method: Method, data: IYoutubeOptions) {
+  return axios({
+    method: method,
+    url: url,
+    params: data
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import * as fastify from 'fastify';
+import * as cors from 'fastify-cors';
 import * as http from 'http';
+
+import * as search from './modules/youtube.api.wrapper';
 
 import { config } from 'dotenv';
 
@@ -9,17 +12,23 @@ const PORT = process.env.PORT;
 
 const server = fastify<http.Server>({ logger: true });
 
+server.register(cors, {
+  origin: true
+});
+
 server.get('/health-check', {}, (request, reply) => {
   reply.code(200).send({ response: 'OK' });
 });
 
-server.get(
-  '/media',
-  {},
-  (request, reply): any => {
-    reply.code(200).send('hello world');
+server.get('/search', {}, async (request, reply) => {
+  const query = request.query.q;
+  try {
+    const result = await search.getYoutubeVideoURL(query);
+    reply.code(200).send(result);
+  } catch (error) {
+    console.log(error);
   }
-);
+});
 
 const start = async () => {
   try {
