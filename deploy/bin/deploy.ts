74920a7d8149b3e41219cd0504c18129740b0f30
Alex Towli <alextowli@hotmail.co.uk>
2020-06-27 23:02:23 +0100

    chore(cdk): deploy stack

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { DeployStack } from '../lib/deploy-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { DeployStack } from '../lib/deploy-stack'
+require('dotenv').config()
 
-const app = new cdk.App();
-new DeployStack(app, 'DeployStack');
+const stackName = process.env.STACK_NAME || ''
+
+const app = new cdk.App()
+
+new DeployStack(app, stackName, {
+  containerName: process.env.CONTAINER_NAME || '',
+  containerTag: process.env.CONTAINER_TAG || '',
+  environment: process.env.ENVIRONMENT || '',
+})
--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,9 +1,48 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as ecs from '@aws-cdk/aws-ecs'
+import * as ecr from '@aws-cdk/aws-ecr'
+
+export interface ICdkStackProps extends cdk.StackProps {
+  containerName: string
+  containerTag: string
+  environment: string
+}
 
 export class DeployStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props: ICdkStackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const cluster = new ecs.Cluster(this, 'radio-backend-cluster')
+
+    /**
+     * Task definition with CloudWatch logging
+     */
+    const logging = new ecs.AwsLogDriver({ streamPrefix: 'radio-backend' })
+
+    const taskDef = new ecs.FargateTaskDefinition(this, 'radio-backend-task-definition', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    })
+
+    const repository = new ecr.Repository(this, 'radio-backend', {
+      repositoryName: 'radio-backend',
+    })
+
+    /**
+     * NB: The first lifecycle rule that matches an image will be applied
+     * against that image.
+     */
+    repository.addLifecycleRule({ maxImageCount: 10 })
+    repository.addLifecycleRule({ maxImageAge: cdk.Duration.days(3) })
+
+    taskDef.addContainer('appContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(repository),
+      logging,
+    })
+
+    new ecs.FargateService(this, 'FargateService', {
+      cluster,
+      taskDefinition: taskDef,
+    })
   }
 }
